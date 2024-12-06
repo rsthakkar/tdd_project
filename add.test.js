963d62adb5dbffbd1dc3b6de4c8ed82c734f3c84
Rudra Thakkar <rsthakkar66@gmail.com>
2024-12-06 09:06:52 +0530

    added test case to check if function is able to handle multiple inputs

--- add.test.js
+++ add.test.js
@@ -12,7 +12,11 @@ test('Single digit in string should calculate the sum', () => {
     expect(add("2")).toBe(2);
 });
 
-test('Add "2,3" and expect return to be 5', () => {
+test('Add "2,3" and expect return value to be 5', () => {
+    expect(add("2,3")).toBe(5);
+});
+
+test('Add "2,3,4,5" and expect return value to be 14', () => {
     expect(add("2,3")).toBe(5);
 });
 
