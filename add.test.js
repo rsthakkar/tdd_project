75175bbbadb255f1eacf6ecbb194bfa2d361517f
Rudra Thakkar <rsthakkar66@gmail.com>
2024-12-06 09:01:00 +0530

    added more test cases for single digit in string and 2 digit in string which passed as code was compatible with inputs

--- add.test.js
+++ add.test.js
@@ -1,9 +1,19 @@
+'use strict';
+
 import add from "./add";
 import { expect, test } from 'vitest'
 
 
 test('empty string should return 0', () => {
-    expect(add("")).toBe(0)
-})
+    expect(add("")).toBe(0);
+});
+
+test('Single digit in string should calculate the sum', () => {
+    expect(add("2")).toBe(2);
+});
+
+test('Add "2,3" and expect return to be 5', () => {
+    expect(add("2,3")).toBe(5);
+});
 
 
